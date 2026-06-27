7c7a865fb787920afdafb99551f23c715f951bc0
Shahriyar Zaman Ridoy <104163801+shahriyar-zaman@users.noreply.github.com>
2026-06-27 19:47:19 +0600

    Delete CAPSTONE directory

--- CAPSTONE/capstone/eval/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Evaluation metrics for POPE and VSR benchmarks."""
-
-from .metrics import compute_metrics, normalize_label, parse_yes_no
-
-__all__ = ["parse_yes_no", "normalize_label", "compute_metrics"]
--- CAPSTONE/capstone/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Shared utilities."""
-
-from .io import json_serializable, save_pil_image
-
-__all__ = ["json_serializable", "save_pil_image"]
